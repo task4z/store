48799be5b388711c3da9616a63a34db32755f8c0
Andre LOURENCO <Andre.LOURENCO@eos-france.com>
2020-04-01 11:22:28 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ShoppingReducer } from './reducers/shopping.reducer';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { ShoppingEffects } from './store/effects/shopping.effects';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    StoreModule.forRoot({
+      shopping: ShoppingReducer
+    }),
+    HttpClientModule,
+    EffectsModule.forRoot([ShoppingEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/models/app-state.model.ts
@@ -0,0 +1,5 @@
+import { ShoppingState } from 'src/app/reducers/shopping.reducer';
+
+export interface AppState {
+  readonly shopping: ShoppingState;
+}
